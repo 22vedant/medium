7065c5fa43ff89a50fe881825fbe5bf95d920a69
vedantchinta <vedantchinta7@gmail.com>
2024-08-05 20:27:40 +0530

    final changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import Card from './Card';
 import NewCard from './NewCard';
 
 interface Post {
--- src/pages/Me.tsx
+++ src/pages/Me.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import axios from 'axios';
 import { useState } from 'react';
-import Card from '../components/Card';
 import NewCard from '../components/NewCard';
 import Topbar from '../components/Topbar';
 
